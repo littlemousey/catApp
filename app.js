9e75a44e65413d8c58f5f522ca980319aaeec07e
Ramon Gebben <ramon@ra-ge.net>
2015-03-28 12:31:55 +0100

    Working

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var request = require('request');
+var app = express();
 
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function( req, res ){
-  res.send('Hello World!')
+  res.send(200)
+})
+
+app.get('/cat', function( req, res ){
+	request('http://thecatapi.com/api/images/get?format=xml&results_per_page=1', function(error, response, body){
+		if (!error && response.statusCode === 200) {
+   	 		res.send( body );
+  		}
+	});
 })
 
 var server = app.listen(3000, function () {
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,28 @@
 
 
-alert("pizza");
+// getImage
+// renderAndReplaceImage
+
+
+function getImage(){
+
+	$.get('/cat', function( data ){
+		renderAndReplaceImage( $( data ).find("url").text() )
+	})
+}
+
+function renderAndReplaceImage( image ){
+	console.log( image );
+	$('#image').attr('src', image )
+}
+
+$(document).ready(function(){
+
+	$('#cat_button').click(function(){
+		console.log('You clicked it!');
+		getImage();
+	});
+
+
+});
+
