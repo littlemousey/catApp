ac24b1598e5bc7191042ab067741341cd30adb52
Ramon Gebben <ramon@ra-ge.net>
2015-03-31 09:08:08 +0200

    added some comments

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.get('/cat', function( req, res ){
 
 // Create server. Hello world example
 var server = app.listen(3000, function () {
-
+	
   var host = server.address().address
   var port = server.address().port
 
